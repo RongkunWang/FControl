bc2f7ca1938258e3009055056bf19e861486a3c1
Rongkun Wang <rongkun.wang@cern.ch>
2022-03-24 16:14:04 +0100

    update the opc xml file naming

--- OpcScaServer.py
+++ OpcScaServer.py
@@ -17,7 +17,7 @@ class OpcScaServer(Server):
     def set_commands(self, det, sector):
         self.run_jobname = f"opc_{sector}"
         self._run_command = f"export PATH={db.OPC_EXE_DIR}:$PATH && "\
-                f"{db.OPC_EXE} {db.OpcPort(db.port_dict[det][sector])} {db.opc_dict[det][sector]}"
+                f"{db.OPC_EXE} {db.OpcPortFile(db.port_dict[det][sector])} {db.opc_dict[det][sector]}"
         self._original_run_command = self.run_command
         self._check_running_command = f"ps aux | grep {db.OPC_EXE} | grep {db.opc_dict[det][sector]} | grep -v grep"
         pass
